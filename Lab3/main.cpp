b905c3d15ea650314a3fe2f95046732e3dcf375b
Алекс <>
2015-12-16 20:57:55 +0200

    Коміт з виконаним завданням

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,9 +1,62 @@
-#include <iostream>
+#include<iostream>
+#include <cstdlib>
+#include <windows.h>
+#include <ctime>
+#include <iomanip>
 
 using namespace std;
+struct Node{
+	int value;
+	Node *left;
+	Node *right;
+};
+Node *root;
+int depth=0 ;
+void InsertTree(Node **aNode, int data){
+	if (!(*aNode)){
+		(*aNode) = new Node;
+		(*aNode)->value = data;
+		(*aNode)->left = NULL;
+		(*aNode)->right = NULL;
+	}
+	else{
+	if (data < (*aNode)->value) InsertTree(&(*aNode)->left,data);
+	else InsertTree(&(*aNode)->right, data);
+	}
+	return;}
+
+void createTree(Node **aNode,int N){
+     srand(time(0));
+    for(int i=0;i<N;i++){
+        int data=rand()%100-50;
+        InsertTree(aNode,data);
+    }
+
+}
+void printTree(Node *aNode){
+	if (aNode){
+		printTree(aNode->left);
+		cout<<aNode->value<<" ";
+		printTree(aNode->right);
+	}
+	return;}
+
+void calculateDepth(Node *aNode,int d){
+     if (aNode){
+        calculateDepth(aNode->left,d+1);
+        calculateDepth(aNode->right,d+1);
+
+     }
+     if (d>depth) depth=d;
+     return ;
+}
+
 
 int main()
-{
-    cout << "Hello world!" << endl;
+{   createTree(&root,10);
+    printTree(root);
+    calculateDepth(root,0);
+    cout<<endl<<depth;
+
     return 0;
 }
